d170c97677fca02452be496e5c232ac1a75351ae
Erwin Korsten <mw.korsten@gmail.com>
2019-01-29 11:10:16 +0100

    Navbar added

--- html/header.php
+++ html/header.php
@@ -2,9 +2,15 @@
 $url = $_SERVER['HTTP_HOST'];
 $url='http://'.$url.'/diceandragons/';
 ?>
+
 <header class="headerwrapper">
 	<div class="logo">
 		<img src="<?php echo $url.'img/logo.png';?>" alt="Dices and dragons" />
 	</div>
-
+	<div class=navbar>
+		<ul class="menu">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+	</div>
 </header>
