3179b9a471d934093d1ad63f756e0ef702092069
Erwin Korsten <mw.korsten@gmail.com>
2019-01-29 10:17:15 +0100

    Logo in header

---
+++ html/header.php
@@ -0,0 +1,10 @@
+<?php
+$url = $_SERVER['HTTP_HOST'];
+$url='http://'.$url.'/diceandragons/';
+?>
+<header class="headerwrapper">
+	<div class="logo">
+		<img src="<?php echo $url.'img/logo.png';?>" alt="Dices and dragons" />
+	</div>
+
+</header>
